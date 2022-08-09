a9b41d50d90725b3ca23ca277e0ec55ef707d7c8
martina asaad <martinamaken@gmail.com>
2022-08-09 10:03:43 -0400

    added System.out statement to GitTest file

---
+++ src/com/java/collectionInterfaces/GitTest.java
@@ -0,0 +1,11 @@
+package com.java.collectionInterfaces;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("git test");
+		System.out.println("git test make change to the file");
+
+	}
+
+}
