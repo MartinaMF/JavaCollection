ed4f8f8c9afa6ddfd95bf393ae2645e1e379fe8e
martina asaad <martinamaken@gmail.com>
2022-08-09 10:09:29 -0400

    made my second change

--- src/com/java/collectionInterfaces/GitTest.java
+++ src/com/java/collectionInterfaces/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("git test");
 		System.out.println("git test make change to the file");
+		System.out.println("git test second change to the file");
 
 	}
 
